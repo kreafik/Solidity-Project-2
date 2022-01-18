a20b15c34384e88bb88461b3fe1ae18084bed323
Cihan <tasarim@cihanduran.com>
2022-01-18 15:59:51 +0300

    Yenilikler

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -34,9 +34,12 @@ const Navbar = () => {
                     : <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)}/>  
                 }
                 {toggleMenu && (
-                    <ul>
+                    <ul
+                        className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+                        flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
+                    >
                         <li className="text-xl w-full my-2">
-                            <AiOutlineClose onClick={() => setToggleMenu=(false)} />
+                            <AiOutlineClose onClick={() =>  setToggleMenu(false)} />
                         </li>
                         {["Market", "Exchange", "Tutorials", "Wallet"].map((item, index) => (
                             <NavbarItem key={item + index} title={item} classProps="my-2 text-lg"/>
